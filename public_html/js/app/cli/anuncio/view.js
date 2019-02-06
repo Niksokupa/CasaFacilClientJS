61d8963f15ea742a8c8ec982fdc80fd49a4e4657
Ramón Lozano <niksokupa@gmail.com>
2019-02-07 00:18:03 +0100

    avances view anuncio, carousel conseguido.

--- public_html/js/app/cli/anuncio/view.js
+++ public_html/js/app/cli/anuncio/view.js
@@ -5,7 +5,7 @@ moduleAnuncio.controller('viewanunciosController', ['$scope', '$http', 'toolServ
         $anchorScroll();
         $scope.terreno = false;
         $scope.id = $routeParams.id;
-        $scope.userId = oSessionService.getId();
+
 
         $http({
             method: 'GET',
@@ -16,19 +16,46 @@ moduleAnuncio.controller('viewanunciosController', ['$scope', '$http', 'toolServ
                 $scope.terreno = true;
             }
 
-
             $scope.anuncio.precio = addCommas($scope.anuncio.precio);
+
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8081/casafacil/json?ob=extras&op=getspecific&id=' + $scope.anuncio.id
+            }).then(function (response) {
+                $scope.extras = response.data.message;
+            }, function (response) {
+                $scope.error = response.data.message || 'Request failed';
+            });
 
         }, function (response) {
             $scope.error = response.data.message || 'Request failed';
         });
 
+        //TODOS LOS EXTRAS
+        $http({
+            method: "GET",
+            url: `http://localhost:8081/casafacil/json?ob=extras&op=getall`
+        }).then(function (response) {
+            var listaExtras = [];
+            response.data.message.forEach(element => {
+                var extras = {
+                    extras: element
+                };
+                listaExtras.push(extras);
+
+            });
+            $scope.listaExtras = listaExtras;
+
+        }), function (response) {
+            console.log(response);
+        };
+
         $http({
             method: 'GET',
             url: 'http://localhost:8081/casafacil/json?ob=fotos&op=getall&id=' + $scope.id
         }).then(function (response) {
+            $scope.userId = response.data.message[0].obj_Anuncio.obj_Usuario.id;
             $scope.fotos = response.data.message;
-
         }, function (response) {
             $scope.error = response.data.message || 'Request failed';
         });
